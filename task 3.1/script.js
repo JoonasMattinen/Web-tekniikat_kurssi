160cf9549a7057b0df96f1934036a81b54ffd531
JoonasMattinen <N2majo02.students.oamk.fi>
2022-12-19 08:58:29 +0200

    added third page

--- task 3.1/script.js
+++ task 3.1/script.js
@@ -1,3 +1,6 @@
 document.getElementById("firstImg").onclick = function(){
     window.location = "./pages/tietoa.html";
 }
+document.getElementById("thirdImg").onclick = function(){
+    window.location = "./pages/puttaus.html";
+}
