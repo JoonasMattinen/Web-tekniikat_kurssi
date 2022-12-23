4d52715834fb79ac9989df492e67d76ce3ce42b0
JoonasMattinen <N2majo02.students.oamk.fi>
2022-12-23 12:48:22 +0200

    deleted files i accidentally uploaded

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("firstImg").onclick = function(){
-    window.location = "./pages/tietoa.html";
-}
-document.getElementById("secondImg").onclick = function(){
-    window.location = "./pages/draivi.html";
-}
-document.getElementById("thirdImg").onclick = function(){
-    window.location = "./pages/puttaus.html";
-}
